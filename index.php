76af36ff4bf5498e82fa696648fd6d6505953411
Sandris <sandrisk32@gmail.com>
2026-02-17 16:34:29 +0200

    Fixed txt and added main files

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MyWiki</title>
+</head>
+<body>
+    <h1>Your own local Wiki</h1>
+    
+</body>
+</html>
