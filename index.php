2c31301ae9181827371959910e52b14e7c6b7590
Sandris <sandrisk32@gmail.com>
2026-02-18 11:27:48 +0200

    Connected database and simple login

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
+<?php
+
+    require("connection.php");
+
+    if(isset($_POST["submit"])) {
+        // var_dump($_POST);
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        $query = "SELECT admins.username, admins.password FROM admins WHERE admins.username = '" . $username  . "'" . "AND admins.password = '" . $password . "'";
+
+        $result = mysqli_query($con, $query);
+
+
+        if (mysqli_num_rows($result) == 0) {
+            echo "<p id='error'>Incorrect username or password</p>";
+        }
+        else {
+            $result_array = mysqli_fetch_assoc($result);
+
+            echo "<p id='test'>Matched rows: " . mysqli_num_rows($result) . "</p>";
+            echo "<p id='test'>Returned value: " . $result_array["username"] . "</p>";
+            echo "<p id='test'>Returned value: " . $result_array["password"] . "</p>";
+
+            header("Location: select.php");
+        }
+            
+        
+
+        mysqli_close($con);
+    }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +39,17 @@
     <title>MyWiki</title>
 </head>
 <body>
+
     <h1>Your own local Wiki</h1>
-    
+
+    <form method="POST" action="">
+
+        <div>
+            <input type="text" placeholder="Username..." name="username"><br>
+            <input type="password" placeholder="Password..." name="password"><br>
+            <input type="submit" name="submit" value="Log in">
+        </div>
+
+    </form>
 </body>
 </html>
--- select.php
+++ select.php
@@ -1,3 +1,18 @@
 <?php
 
     require("connection.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <title>MyWiki</title>
+</head>
+<body>
+    <h1>What are you looking for?</h1>
+
+
+</body>
+</html>
