a987e89892b666ad25b605d48764c50ec8b74e9b
Sandris <sandrisk32@gmail.com>
2026-02-23 15:04:46 +0200

    Added search feature, improved css

--- select.php
+++ select.php
@@ -1,11 +1,23 @@
 <?php
+
     require("connection.php");
 
     $stmt = $con->prepare("SELECT * FROM articles LIMIT 5");
 
     $stmt->execute();
-
     $result = $stmt->get_result();
+
+    $search_result = "";
+
+    if (isset($_GET["search"])) {
+        $keyword = "%" . $_GET["search"] . "%";
+        $stmt = $con->prepare("SELECT * FROM articles WHERE content LIKE ?");
+
+        $stmt->bind_param("s", $keyword);
+        $stmt->execute();
+
+        $search_result = $stmt->get_result();
+    }
 
 ?>
 <!DOCTYPE html>
@@ -20,6 +32,14 @@
 
     <h1>What are you looking for?</h1>
 
+    <div class="search_field">
+        <form method="GET" action="select.php">
+            <input type="text" id="input_field" name="search" placeholder="A article about..."> 
+        </form>
+    </div>
+
+    <br>
+
     <div class="header">
         <div class="action_box2">
             <a class="normal_link" href="create.php">
@@ -27,28 +47,50 @@
             </a>
         </div>    
     </div>
-    
-    <?php 
+
+    <div class="results_box">
+        <?php
+        
+            if ($search_result) {   
+                if ($search_result->num_rows > 0) {
+                    echo "Search results:";
+                    while ($found_result = $search_result->fetch_assoc()) {
+        ?>
+                        <a href="article.php?id=<?php echo htmlspecialchars($found_result["id"]); ?>"><?= htmlspecialchars($found_result["title"]) ?></a>
+                        <hr>
+        <?php
+                    }
+                }
+            };
+        ?>
+    </div>
+
+    <?php
 
         if ($result->num_rows == 0) {
             echo "<p>No articles yet. Create one!</p>";
         }
 
-        while ($row = $result->fetch_assoc()) {
-            echo "<div class='article_box'>";
-                echo "<h2><a href='article.php?id=" . $row["id"] . "'>" . htmlspecialchars($row["title"]) . "</a></h2>";
-                $full_content = $row["content"];
-                if (mb_strlen($full_content) > 200) { // Checks if the full text is bigger thatn 200 characters
-                    $preview_content = mb_substr($row["content"], 0, 200) . "..."; // Creates the preview text
-                    echo "<div class='article_text article_margin_left'>" . nl2br(htmlspecialchars($preview_content)) . "</div>";
-                }
-                else {
-                    echo "<div class='article_text article_margin_left'>" . htmlspecialchars($full_content) . "</div>";
-                }
-                
-            echo "</div>";
-        }
-        
     ?>
+    
+    <div class="article_box">
+        <?php
+            while ($row = $result->fetch_assoc()) {
+                echo "<div class='article_box'>";
+                    echo "<h2><a href='article.php?id=" . $row["id"] . "'>" . htmlspecialchars($row["title"]) . "</a></h2>";
+                    $full_content = $row["content"];
+                    if (mb_strlen($full_content) > 200) { // Checks if the full text is bigger thatn 200 characters
+                        $preview_content = mb_substr($row["content"], 0, 250) . "..."; // Creates the preview text
+                        echo "<div class='article_text article_margin_left'>" . nl2br(htmlspecialchars($preview_content)) . "</div>";
+                    }
+                    else {
+                        echo "<div class='article_text article_margin_left'>" . htmlspecialchars($full_content) . "</div>";
+                    }
+                    
+                echo "</div>";
+            }
+            
+        ?>
+    </div>
 </body>
 </html>
