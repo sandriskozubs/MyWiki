01e7d157ed20003dc655ab23b7f0bd721b71c995
Sandris <sandrisk32@gmail.com>
2026-02-27 13:40:45 +0200

    Refactored index.php

--- index.php
+++ index.php
@@ -3,33 +3,36 @@
     require("connection.php");
 
     if ($_SERVER["REQUEST_METHOD"] === "POST") {
-        // var_dump($_POST);
         
         $username = $_POST["username"];
         $password = $_POST["password"];
 
-        $stmt = $con->prepare("SELECT username, password FROM admins WHERE username = ?");
-        $stmt->bind_param("s", $username);
-        $stmt->execute();
-
-        $result = $stmt->get_result();
-
-        if ($result->num_rows == 0) {
-            echo "<p id='error'><b>!!</b> Incorrect username or password</p>";
+        if (empty($username) || empty($password)) {
+            echo "<p id='error'><b>!!</b> Dont leave the fields empty!</p>";
+            mysqli_close($con);
         }
         else {
-            $row = $result->fetch_assoc();
+            $stmt = $con->prepare("SELECT username, password FROM admins WHERE username = ?");
+            $stmt->bind_param("s", $username);
+            $stmt->execute();
 
-            if (password_verify($password, $row["password"]) && $row["username"] == $username) {
-                header("Location: select.php");
-                exit;
-            }
-            else {
+            $result = $stmt->get_result();
+
+            if ($result->num_rows == 0) {
                 echo "<p id='error'><b>!!</b> Incorrect username or password</p>";
             }
+            else {
+                $row = $result->fetch_assoc();
+                if (password_verify($password, $row["password"]) && $row["username"] == $username) {
+                    header("Location: select.php");
+                    exit;
+                }
+                else {
+                    echo "<p id='error'><b>!!</b> Incorrect username or password</p>";
+                }
+            }
+            mysqli_close($con);
         }
-        
-        mysqli_close($con);
     }
 
 ?>
@@ -42,7 +45,6 @@
     <title>MyWiki</title>
 </head>
 <body>
-
     <div class="login_header">
         <h1>MyWiki</h1>
     </div>
@@ -52,13 +54,11 @@
     </div>
 
     <form method="POST" action="">
-
         <div class="login_box">
             <input required type="text" id="input_field" placeholder="Username..." name="username">
             <input required type="password" id="input_field" placeholder="Password..." name="password">
             <input type="submit" id="action_login" name="submit" value="Login">
         </div>
-
     </form>
 </body>
 </html>
